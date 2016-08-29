5edb7470a76d32ef201fbf9c58e16cd3bb32a68d
Manuel Pietschmann <mpi@dresden-elektronik.de>
2016-08-29 16:45:10 +0200

    Support older compilers

--- rest_configuration.cpp
+++ rest_configuration.cpp
@@ -500,7 +500,7 @@ int DeRestPluginPrivate::getConfig(const ApiRequest &req, ApiResponse &rsp)
         return REQ_READY_SEND;
     }
 
-    rsp.hdrFields.append(qMakePair(QLatin1String("Access-Control-Allow-Origin"), QLatin1String("*")));
+    rsp.hdrFields.append(qMakePair(QString(QLatin1String("Access-Control-Allow-Origin")), QString(QLatin1String("*"))));
     checkRfConnectState();
 
     // handle ETag
