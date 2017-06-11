25a2a3bf09faca9bbaefe50e3537a2d95a30f80d
Manuel Pietschmann <mpi@dresden-elektronik.de>
2017-06-11 12:50:43 +0200

    Allow authorization header in http preflight request

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -8647,7 +8647,7 @@ int DeRestPlugin::handleHttpRequest(const QHttpRequestHeader &hdr, QTcpSocket *s
         stream << "Access-Control-Allow-Origin: *\r\n";
         stream << "Access-Control-Allow-Credentials: true\r\n";
         stream << "Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE\r\n";
-        stream << "Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type\r\n";
+        stream << "Access-Control-Allow-Headers: Authorization, Access-Control-Allow-Origin, Content-Type\r\n";
         stream << "Access-Control-Expose-Headers: Gateway-Name, Gateway-Uuid\r\n";
         stream << "Content-Type: text/html\r\n";
         stream << "Content-Length: 0\r\n";
