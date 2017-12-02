eb06ca03d9a6a9e6418d5b36a7b737ed309a0d06
Manuel Pietschmann <mpi@dresden-elektronik.de>
2017-12-02 13:25:33 +0100

    Support Xiaomi Aquara wall switch QBKG03LM (experimental)
    Issue https://github.com/dresden-elektronik/deconz-rest-plugin/issues/165

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -114,6 +114,7 @@ static const SupportedDevice supportedDevices[] = {
     { VENDOR_JENNIC, "lumi.sensor_cube", jennicMacPrefix },
     { VENDOR_JENNIC, "lumi.sensor_86sw1", jennicMacPrefix },
     { VENDOR_JENNIC, "lumi.sensor_86sw2", jennicMacPrefix },
+    { VENDOR_JENNIC, "lumi.ctrl_neural2", jennicMacPrefix },
     { VENDOR_UBISYS, "D1", ubisysMacPrefix },
     { VENDOR_NONE, "Z716A", netvoxMacPrefix },
     { 0, 0, 0 }
@@ -2568,7 +2569,8 @@ void DeRestPluginPrivate::addSensorNode(const deCONZ::Node *node)
                         fpOpenCloseSensor.inClusters.push_back(ci->id());
                     }
                     else if (modelId == QLatin1String("lumi.sensor_86sw1") ||
-                             modelId == QLatin1String("lumi.sensor_86sw2"))
+                             modelId == QLatin1String("lumi.sensor_86sw2") ||
+                             modelId == QLatin1String("lumi.ctrl_neural2"))
                     {
                         if (i->endpoint() == 0x01) // create sensor only for first endpoint
                         {
@@ -3409,7 +3411,8 @@ void DeRestPluginPrivate::updateSensorNode(const deCONZ::NodeEvent &event)
                 if ((event.node()->address().ext() & macPrefixMask) == jennicMacPrefix)
                 {
                     if (i->modelId() == QLatin1String("lumi.sensor_86sw1") ||
-                        i->modelId() == QLatin1String("lumi.sensor_86sw2"))
+                        i->modelId() == QLatin1String("lumi.sensor_86sw2") ||
+                        i->modelId() == QLatin1String("lumi.ctrl_neural2"))
                     { // 3 endpoints: 1 sensor
                     }
                     else
@@ -3898,7 +3901,8 @@ void DeRestPluginPrivate::updateSensorNode(const deCONZ::NodeEvent &event)
                                     quint32 button;
 
                                     if (i->modelId() == QLatin1String("lumi.sensor_86sw1") ||
-                                        i->modelId() == QLatin1String("lumi.sensor_86sw2"))
+                                        i->modelId() == QLatin1String("lumi.sensor_86sw2") ||
+                                        i->modelId() == QLatin1String("lumi.ctrl_neural2"))
                                     {
                                         button = (S_BUTTON_1 * event.endpoint()) + S_BUTTON_ACTION_SHORT_RELEASED;
                                     }
