5ce7b7536f58a60dc8483e9865659ba574279329
Manuel Pietschmann <mpi@dresden-elektronik.de>
2018-08-30 16:48:03 +0200

    Add missing nwk parameter in sql insert

--- database.cpp
+++ database.cpp
@@ -411,7 +411,7 @@ void DeRestPluginPrivate::refreshDeviceDb(const deCONZ::Address &addr)
 
     QString sql = QString(QLatin1String(
                               "UPDATE devices SET timestamp = strftime('%s','now'), nwk = %2 WHERE mac = '%1';"
-                              "INSERT INTO devices (mac,nwk,timestamp) SELECT '%1', strftime('%s','now') WHERE (SELECT changes() = 0);"))
+                              "INSERT INTO devices (mac,nwk,timestamp) SELECT '%1', %2, strftime('%s','now') WHERE (SELECT changes() = 0);"))
             .arg(generateUniqueId(addr.ext(), 0, 0)).arg(addr.nwk());
     dbQueryQueue.push_back(sql);
 
