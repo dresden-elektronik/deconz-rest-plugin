5d82bd291045e95bc29060ffbda83ae161e47081
ma-ca <ma-ca@users.noreply.github.com>
2019-01-08 20:36:13 +0100

    Add support for Bitron magnetic contact 902010/21
    model identifier:
    - 902010/21
    - 902010/21A
    - 902010/21B
    
    Changed supported device from 902010/21A to 902010/21 in order to also support 902010/21B see issue #1100

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -104,7 +104,7 @@ static const SupportedDevice supportedDevices[] = {
     { VENDOR_NONE, "BX_", tiMacPrefix }, // Climax siren
     { VENDOR_NONE, "PSMD_", tiMacPrefix }, // Climax smart plug
     { VENDOR_NONE, "OJB-IR715-Z", tiMacPrefix },
-    { VENDOR_NONE, "902010/21A", tiMacPrefix }, // Bitron: door/window sensor
+    { VENDOR_NONE, "902010/21", tiMacPrefix }, // Bitron: door/window sensor
     { VENDOR_NONE, "902010/24", tiMacPrefix }, // Bitron: smoke detector
     { VENDOR_NONE, "902010/25", tiMacPrefix }, // Bitron: smart plug
     { VENDOR_BITRON, "902010/32", emberMacPrefix }, // Bitron: thermostat
