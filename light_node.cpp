7de6716aa5cd3776204f744541a7f51945875a1c
ebaauw <erik.baauw@xs4all.nl>
2019-04-26 16:43:54 +0200

    Update light_node.cpp
    Bug fix: whitelist to suppress `state.bri` for on/off plugs with _Level_ cluster was ignored.  See #1444.

--- light_node.cpp
+++ light_node.cpp
@@ -360,6 +360,7 @@ void LightNode::setHaEndpoint(const deCONZ::SimpleDescriptor &endpoint)
                     if ((manufacturerCode() == VENDOR_IKEA && endpoint.deviceId() == DEV_ID_Z30_ONOFF_PLUGIN_UNIT) || // IKEA Tradfri control outlet
                         (manufacturerCode() == VENDOR_INNR && endpoint.deviceId() == DEV_ID_ZLL_ONOFF_PLUGIN_UNIT)) // innr SP120 smart plug
                     { } // skip state.bri not supported
+                    else
                     {
                         addItem(DataTypeUInt8, RStateBri);
                     }
