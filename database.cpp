9330fc12b99b549d7ab35662e5f505864b842aaf
Marcin <merdok@users.noreply.github.com>
2020-01-01 16:58:13 +0100

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -3322,7 +3322,7 @@ static int sqliteLoadAllSensorsCallback(void *user, int ncols, char **colval , c
                 sensor.modelId() != QLatin1String("lumi.sensor_switch") &&
                 !sensor.modelId().contains(QLatin1String("weather")) &&
                 !sensor.modelId().startsWith(QLatin1String("lumi.sensor_ht")) &&
-                sensor.modelId().contains(QLatin1String("86opcn01")) == false) // exclude Aqara Opple
+                !sensor.modelId().contains(QLatin1String("86opcn01"))) // exclude Aqara Opple
             {
                 item = sensor.addItem(DataTypeInt16, RConfigTemperature);
                 item->setValue(0);
