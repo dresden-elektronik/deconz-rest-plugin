42011c6b14ab10996d0db406a0dea7b4f310658f
ebaauw <erik.baauw@xs4all.nl>
2020-02-18 18:03:35 +0100

    Update basic.cpp
    Implement _Basic_ cluster attributes for coordinator.
    - Fix _ZCL Version_;
    - Add _Application Version_, _Stack Version_, _HW Version_;
    - Add _Manufacturer Name_: set to `dresden elektronik`;
    - Add_ Model Identifier_: set to device name;
    - Add _Power Source_: set to _DC Power_;
    - Add _SW Build ID_: set to device firmware version.

--- basic.cpp
+++ basic.cpp
@@ -12,7 +12,6 @@
 #include "de_web_plugin.h"
 #include "de_web_plugin_private.h"
 
-
 /*! Handle packets related to the ZCL Basic cluster.
     \param ind the APS level data indication containing the ZCL packet
     \param zclFrame the actual ZCL frame which holds the read attribute request
@@ -49,7 +48,7 @@ void DeRestPluginPrivate::sendBasicClusterResponse(const deCONZ::ApsDataIndicati
                                 deCONZ::ZclFCDisableDefaultResponse);
 
     //is there manufacture field in the request, if yes add it.
-    if (zclFrame.frameControl() & deCONZ::ZclFCManufacturerSpecific) 
+    if (zclFrame.frameControl() & deCONZ::ZclFCManufacturerSpecific)
     {
         quint16 manucode = zclFrame.manufacturerCode();
         outZclFrame.setFrameControl(outZclFrame.frameControl() | deCONZ::ZclFCManufacturerSpecific);
@@ -70,20 +69,88 @@ void DeRestPluginPrivate::sendBasicClusterResponse(const deCONZ::ApsDataIndicati
             instream >> attr;
             stream << attr;
 
-            switch(attr)
+            switch (attr)
             {
-            case 0x0000:
-                //ZCL version
+            case 0x0000: // ZCL Version
                 stream << code;
                 stream << (quint8) deCONZ::Zcl8BitUint;
-                stream << 0x02;
+                stream << (quint8) 0x02;
                 break;
 
-            case 0xF000:
-                //Legrand attribute used for pairing
+            case 0x0001: // Application Version
+                stream << code;
+                stream << (quint8) deCONZ::Zcl8BitUint;
+                stream << (quint8) 0x00;
+                break;
+
+            case 0x0002: // Stack Version
+                stream << code;
+                stream << (quint8) deCONZ::Zcl8BitUint;
+                stream << (quint8) 0x00;
+                break;
+
+            case 0x0003: // HW Version
+                stream << code;
+                stream << (quint8) deCONZ::Zcl8BitUint;
+                stream << (quint8) 0x00;
+                break;
+
+            case 0x0004: // Manufacturer Name
+            {
+                const char *name = "dresden elektronik";
+                const quint8 length = strlen(name);
+
+                stream << code;
+                stream << (quint8) deCONZ::ZclCharacterString;
+                stream << length;
+                for (uint i = 0; i < length; i++)
+                {
+                    stream << (quint8) name[i];
+                }
+            }
+                break;
+
+            case 0x0005: // Model Identifier
+            {
+                const QByteArray id = gwDeviceName.toLatin1();
+                const quint8 length = id.length();
+
+                stream << code;
+                stream << (quint8) deCONZ::ZclCharacterString;
+                stream << length;
+                for (uint i = 0; i < length; i++)
+                {
+                    stream << (quint8) id[i];
+                }
+            }
+                break;
+
+            case 0x0007: // Power Source
+                stream << code;
+                stream << (quint8) deCONZ::Zcl8BitEnum;
+                stream << (quint8) 0x04; // DC Power
+                break;
+
+            case 0x4000: // SW Build ID
+            {
+                const QByteArray version = gwFirmwareVersion.toLatin1();
+                const quint8 length = version.length();
+
+                stream << code;
+                stream << (quint8) deCONZ::ZclCharacterString;
+                stream << length;
+                for (uint i = 0; i < length; i++)
+                {
+                    stream << (quint8) version[i];
+                }
+            }
+                break;
+
+            case 0xF000: // Legrand attribute used for pairing
+                // FIXME: check Legrand manufacturer code
                 stream << code;
                 stream << (quint8) deCONZ::Zcl32BitUint;
-                stream << 0x000000d5;
+                stream << (quint32) 0x000000d5;
                 break;
 
             default:
