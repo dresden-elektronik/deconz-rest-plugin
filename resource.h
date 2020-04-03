b224f4f09f5dbd3711d188d44ab8b3b6e88d1aad
ebaauw <erik.baauw@xs4all.nl>
2020-04-03 20:07:12 +0200

    Update resource.h
    Add `lastannounced` and `lastseen` attributes, see 2590.

--- resource.h
+++ resource.h
@@ -47,6 +47,8 @@ extern const char *RAttrType;
 extern const char *RAttrClass;
 extern const char *RAttrUniqueId;
 extern const char *RAttrSwVersion;
+extern const char *RAttrLastAnnounce;
+extern const char *RAttrLastSeen;
 
 extern const char *RActionScene;
 
