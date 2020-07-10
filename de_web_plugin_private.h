05d2139afaf608738736e6bfd1f71315fdb8bc12
Andreas Scherbaum <andreas@scherbaum.la>
2020-07-10 13:01:14 +0200

    Remove DeRestPluginPrivate::clearDb()
    According to #2951 this function is no longer used
    Fix: #2951

--- de_web_plugin_private.h
+++ de_web_plugin_private.h
@@ -1421,7 +1421,6 @@ public:
     void refreshDeviceDb(const deCONZ::Address &addr);
     void pushZdpDescriptorDb(quint64 extAddress, quint8 endpoint, quint16 type, const QByteArray &data);
     void pushZclValueDb(quint64 extAddress, quint8 endpoint, quint16 clusterId, quint16 attributeId, qint64 data);
-    void clearDb();
     void openDb();
     void readDb();
     void loadAuthFromDb();
