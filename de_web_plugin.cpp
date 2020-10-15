9915662d81f54c80b5b4f6f4c9b3c79858ba8c5d
Manuel Pietschmann <mpi@dresden-elektronik.de>
2020-10-15 17:47:03 +0200

    Don't reactivate deleted ZGP switches

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -1515,7 +1515,7 @@ void DeRestPluginPrivate::gpDataIndication(const deCONZ::GpDataIndication &ind)
             }
         }
 
-        if (!sensor)
+        if (!sensor || sensor->deletedState() == Sensor::StateDeleted)
         {
             if (searchSensorsState != SearchSensorsActive)
             {
@@ -1589,24 +1589,6 @@ void DeRestPluginPrivate::gpDataIndication(const deCONZ::GpDataIndication &ind)
 
             indexRulesTriggers();
             gpProcessButtonEvent(ind);
-        }
-        else if (sensor && sensor->deletedState() == Sensor::StateDeleted)
-        {
-            if (searchSensorsState == SearchSensorsActive)
-            {
-                sensor->setDeletedState(Sensor::StateNormal);
-                checkSensorGroup(sensor);
-                sensor->setNeedSaveDatabase(true);
-                sensor->rx();
-                DBG_Printf(DBG_INFO, "SensorNode %u: %s reactivated\n", sensor->id().toUInt(), qPrintable(sensor->name()));
-                updateSensorEtag(sensor);
-
-                Event e(RSensors, REventAdded, sensor->id());
-                enqueueEvent(e);
-                queSaveDb(DB_SENSORS , DB_SHORT_SAVE_DELAY);
-
-                gpProcessButtonEvent(ind);
-            }
         }
         else if (sensor && sensor->deletedState() == Sensor::StateNormal)
         {
