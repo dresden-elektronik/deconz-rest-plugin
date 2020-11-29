ed80334d89ce3f90684cffe212f9818b950a5ec7
SwoopX <rol_swoop@gmx.de>
2020-11-29 11:46:04 +0100

    Allow clearing the alarm bit for Develco devices
    - Revert previous change in this regard
    - Enable sending default responses for cluster 0x0500 if requested

--- ias_zone.cpp
+++ ias_zone.cpp
@@ -83,6 +83,12 @@ void DeRestPluginPrivate::handleIasZoneClusterIndication(const deCONZ::ApsDataIn
             return; // sanity
         }
     }
+    
+    // Allow clearing the alarm bit for Develco devices
+    if (!(zclFrame.frameControl() & deCONZ::ZclFCDisableDefaultResponse))
+    {
+        sendZclDefaultResponse(ind, zclFrame, deCONZ::ZclSuccessStatus);
+    }
 
     if ((zclFrame.commandId() == CMD_STATUS_CHANGE_NOTIFICATION && zclFrame.isClusterCommand()) || attrId == IAS_ZONE_CLUSTER_ATTR_ZONE_STATUS_ID)
     {
