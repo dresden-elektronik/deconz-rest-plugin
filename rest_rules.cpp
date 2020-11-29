a9fe7bd118c48d058593fc57e210e70f77a316eb
SwoopX <rol_swoop@gmx.de>
2020-11-29 17:18:13 +0100

    Change error type of rule name exceeding maximum length

--- rest_rules.cpp
+++ rest_rules.cpp
@@ -441,7 +441,7 @@ int DeRestPluginPrivate::createRule(const ApiRequest &req, ApiResponse &rsp)
         {
             if (name.size() > MAX_RULE_NAME_LENGTH)
             {
-                rsp.list.append(errorToMap(ERR_MISSING_PARAMETER, QString("/rules/name"), QString("invalid/missing parameters in body")));
+                rsp.list.append(errorToMap(ERR_INVALID_VALUE, QString("/rules/name"), QString("invalid/missing parameters in body")));
                 rsp.httpStatus = HttpStatusBadRequest;
                 return REQ_READY_SEND;
             }
