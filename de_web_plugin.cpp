eb1eff4c31c641abdc5f4d2b09a6405fb6d99278
Smanar <aurelien.pra@laposte.net>
2021-01-15 20:06:38 +0100

    first test

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -2219,8 +2219,9 @@ void DeRestPluginPrivate::addLightNode(const deCONZ::Node *node)
             {
                 hasServerOnOff = false;
             }
-            //to test
-            if (lightNode.manufacturer() == QLatin1String("_TZE200_wmcdj3aq"))
+            //Battery covering
+            if ((lightNode.manufacturer() == QLatin1String("_TZE200_wmcdj3aq")) ||
+                (lightNode.manufacturer() == QLatin1String("_TZE200_zah67ekd")) )
             {
                 hasServerOnOff = true;
             }
@@ -2551,6 +2552,7 @@ void DeRestPluginPrivate::addLightNode(const deCONZ::Node *node)
         if ((lightNode.manufacturer() == QString("_TYST11_xu1rkty3")) ||
             (lightNode.manufacturer() == QString("_TZE200_xuzcvlku")) ||
             (lightNode.manufacturer() == QString("_TZE200_wmcdj3aq")) ||
+            (lightNode.manufacturer() == QString("_TZE200_zah67ekd")) ||
             (lightNode.manufacturer() == QString("_TYST11_wmcdj3aq")) )
         {
             lightNode.addItem(DataTypeBool, RStateOpen);
