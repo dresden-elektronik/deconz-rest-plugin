096bedfcea17d184afec766a3af548bf90898857
Smanar <aurelien.pra@laposte.net>
2021-02-12 18:58:22 +0100

    Adding _TZ3000_arfwfgoa

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -1056,6 +1056,7 @@ void DeRestPluginPrivate::apsdeDataIndication(const deCONZ::ApsDataIndication &i
                         (sensorNode->manufacturer() == QLatin1String("_TZ3000_qzjcsmar")) ||
                         (sensorNode->manufacturer() == QLatin1String("_TZ3000_owgcnkrh")) ||
                         (sensorNode->manufacturer() == QLatin1String("_TZ3000_adkvzooy")) ||
+                        (sensorNode->manufacturer() == QLatin1String("_TZ3000_arfwfgoa")) ||
                         (sensorNode->manufacturer() == QLatin1String("_TYZB02_keyjqthh")))
                     {
                         sensorNode = getSensorNodeForAddressAndEndpoint(ind.srcAddress(), 0x01);
@@ -2238,6 +2239,7 @@ void DeRestPluginPrivate::addLightNode(const deCONZ::Node *node)
                 (lightNode.manufacturer() == QLatin1String("_TZ3000_peszejy7")) ||
                 (lightNode.manufacturer() == QLatin1String("_TZ3000_qzjcsmar")) ||
                 (lightNode.manufacturer() == QLatin1String("_TZ3000_owgcnkrh")) ||
+                (lightNode.manufacturer() == QLatin1String("_TZ3000_arfwfgoa")) ||
                 (lightNode.manufacturer() == QLatin1String("_TYZB02_keyjqthh")))
             {
                 hasServerOnOff = false;
@@ -5046,6 +5048,7 @@ void DeRestPluginPrivate::addSensorNode(const deCONZ::Node *node, const deCONZ::
                              (manufacturer == QLatin1String("_TZ3000_qzjcsmar")) ||
                              (manufacturer == QLatin1String("_TZ3000_owgcnkrh")) ||
                              (manufacturer == QLatin1String("_TZ3000_adkvzooy")) ||
+                             (manufacturer == QLatin1String("_TZ3000_arfwfgoa")) ||
                              (manufacturer == QLatin1String("_TYZB02_keyjqthh")) )
                     {
                         //Making the device only for endpoint 0x01
