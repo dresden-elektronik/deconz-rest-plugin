9215fc7c094d036b6b321e97f59f2ac411781776
Smanar <aurelien.pra@laposte.net>
2021-02-15 17:05:08 +0100

    Space correction

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -2237,7 +2237,7 @@ void DeRestPluginPrivate::addLightNode(const deCONZ::Node *node)
             }
             //Battery covering
             if ((lightNode.manufacturer() == QLatin1String("_TZE200_wmcdj3aq")) ||
-                (lightNode.manufacturer() == QLatin1String("_TZE200_zah67ekd")) ) // MoesHouse / Livolo Roller Blinds
+                (lightNode.manufacturer() == QLatin1String("_TZE200_zah67ekd"))) // MoesHouse / Livolo Roller Blinds
             {
                 hasServerOnOff = true;
             }
