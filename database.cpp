aed576e7d6589a1588eab537b7b5c0db360f0824
Manuel Pietschmann <mpi@dresden-elektronik.de>
2021-03-29 22:08:26 +0200

    Device: Ignore Daylight and CLIP sensors

--- database.cpp
+++ database.cpp
@@ -4236,9 +4236,12 @@ static int sqliteLoadAllSensorsCallback(void *user, int ncols, char **colval , c
                 d->sensors.push_back(sensor);
                 d->updateSensorEtag(&d->sensors.back());
 
-                const auto key = extAddr != 0 ? extAddr : qHash(sensor.uniqueId());
-                auto *device = DEV_GetOrCreateDevice(d, d->m_devices, key);
-                device->addSubDevice(&d->sensors.back());
+                if (!isClip && sensor.modelId() != QLatin1String("Daylight"))
+                {
+                    const auto key = extAddr != 0 ? extAddr : qHash(sensor.uniqueId());
+                    auto *device = DEV_GetOrCreateDevice(d, d->m_devices, key);
+                    device->addSubDevice(&d->sensors.back());
+                }
 
                 if (sensor.needSaveDatabase())
                 {
