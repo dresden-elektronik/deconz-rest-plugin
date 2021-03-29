2134809ed034bf2cade2cfd88eaaf987d620f4e8
Manuel Pietschmann <mpi@dresden-elektronik.de>
2021-03-29 16:41:54 +0200

    Device sources: Add BSD license comment

--- device_descriptions.cpp
+++ device_descriptions.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021 dresden elektronik ingenieurtechnik gmbh.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ *
+ */
+
 #include <QDirIterator>
 #include <QFile>
 #include <QJsonArray>
