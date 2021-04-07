3fe847c053ba9464df1e8cb354039b83eac08228
Manuel Pietschmann <mpi@dresden-elektronik.de>
2021-04-07 22:56:39 +0200

    Add script to count source lines in Device code

---
+++ cloc_device.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cloc \
+	device.* \
+	device_access_fn.* \
+	device_compat.* \
+	device_ddf_init.* \
+	device_descriptions.* \
+	device_js.* \
+	device_js_wrappers.* \
+	device_tick.* \
+	state_change.*
+
