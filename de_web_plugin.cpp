6f2d3c8b21ccdb6a05e105fc27031f9962d5b4c0
SwoopX <swoop0711@gmx.de>
2021-04-23 12:16:02 +0200

    Offload some checks to a dedicated function

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -9781,20 +9781,47 @@ Sensor *DeRestPluginPrivate::getSensorNodeForAddressAndEndpoint(const deCONZ::Ad
 
 }
 
-/*! Returns the first Sensor for its given \p Address and \p Endpoint and \p Cluster or 0 if not found.
+/*! Compare addresses where either NWK or MAC address might not be known.
+    \returns true if both adresses have same MAC address (strong guaranty).
+    \returns true if at least one of the addresses doesn't have MAC but the NWK addresses are equal.
+    \returns false otherwise.
+*/
+bool isSameAddress(const deCONZ::Address &a, const deCONZ::Address &b)
+{
+    if (a.hasExt() && b.hasExt())
+    {
+        // nested if statement, so the NWK check won't be made if both MAC addresses are known
+        if (a.ext() != b.ext()) 
+        {
+             return false;
+        }
+    }
+    else  if (a.hasNwk() && b.hasNwk())
+    {
+       if (a.nwk() != b.nwk())
+       {
+            return false;
+       }
+    }
+    else { return false; }
+
+    return true;
+}
+
+/*! Returns the first Sensor for its given \p Address and \p Endpoint and \p Cluster or nullptr if not found.
  */
 Sensor *DeRestPluginPrivate::getSensorNodeForAddressEndpointAndCluster(const deCONZ::Address &addr, quint8 ep, quint16 cluster)
 {
     for (Sensor &sensor: sensors)
     {
-        if (sensor.deletedState() != Sensor::StateNormal || !sensor.node())                                     { continue; }
-        if (!((sensor.address().hasExt() && addr.hasExt() && sensor.address().ext() == addr.ext()) ||
-            (sensor.address().hasNwk() && addr.hasNwk() && sensor.address().nwk() == addr.nwk())))              { continue; }
-        if (!((sensor.fingerPrint().hasInCluster(cluster) || sensor.fingerPrint().hasOutCluster(cluster))))     { continue; }
-        if (sensor.fingerPrint().endpoint != ep)                                                                { continue; }
-        else { return &sensor; }
-    }
+        if (sensor.deletedState() != Sensor::StateNormal || !sensor.node())                             { continue; }
+        if (!isSameAddress(sensor.address(), addr))                                                     { continue; }
+        if (sensor.fingerPrint().hasInCluster(cluster) || sensor.fingerPrint().hasOutCluster(cluster))  { }
+        else                                                                                            { continue; }
 
+        if (sensor.fingerPrint().endpoint != ep)    { continue; }
+        else                                        { return &sensor; }
+    }
     return nullptr;
 }
 
