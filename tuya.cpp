dd5e7779c0ffeae66f3a8fd10e561fabccd9f1a7
Smanar <aurelien.pra@laposte.net>
2021-05-04 19:18:42 +0200

    Update tuya.cpp

--- tuya.cpp
+++ tuya.cpp
@@ -104,7 +104,7 @@ void DeRestPluginPrivate::handleTuyaClusterIndication(const deCONZ::ApsDataIndic
     
     const auto productId = R_GetProductId(lightNode);
     
-    // DBG_Printf(DBG_INFO, "Tuya debug 4 : Address 0x%016llX, Command 0x%02X, Payload %s\n", ind.srcAddress().ext(), zclFrame.commandId(), qPrintable(zclFrame.payload().toHex()));
+    DBG_Printf(DBG_INFO, "Tuya debug 3 : Address 0x%016llX, Command 0x%02X, Payload %s\n", ind.srcAddress().ext(), zclFrame.commandId(), qPrintable(zclFrame.payload().toHex()));
     
     // Send default response, it seem at least 0x01 and 0x02 need defaut response
     if ((zclFrame.commandId() == TUYA_REPORTING || zclFrame.commandId() == TUYA_QUERY)&& !(zclFrame.frameControl() & deCONZ::ZclFCDisableDefaultResponse))
@@ -901,6 +901,22 @@ void DeRestPluginPrivate::handleTuyaClusterIndication(const deCONZ::ApsDataIndic
                         {
                             item->setValue(bat);
                             Event e(RSensors, RStateLowBattery, sensorNode->id(), item);
+                            enqueueEvent(e);
+                            update = true;
+                        }
+                    }
+                    break;
+                    case 0x0170: // Reporting
+                    {
+                        bool reporting = false;
+                        if (data == 1) { reporting = true; }
+
+                        ResourceItem *item = sensorNode->item(RConfigReporting);
+
+                        if (item && item->toBool() != reporting)
+                        {
+                            item->setValue(reporting);
+                            Event e(RSensors, RConfigReporting, sensorNode->id(), item);
                             enqueueEvent(e);
                             update = true;
                         }
