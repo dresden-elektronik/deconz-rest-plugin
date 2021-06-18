c1bfef3ee5183f03d5c0d9fe2640297b801b3f64
Smanar <aurelien.pra@laposte.net>
2021-06-18 18:06:35 +0200

    Clean

--- ias_ace.cpp
+++ ias_ace.cpp
@@ -5,28 +5,6 @@
 #include "json.h"
 #include "ias_ace.h"
 
-// server send
-#define CMD_ARM_RESPONSE 0x00
-#define CMD_GET_ZONE_ID_MAP_RESPONSE 0x01
-#define CMD_GET_ZONE_INFORMATION_RESPONSE 0x02
-#define CMD_ZONE_STATUS_CHANGED 0x03
-#define CMD_PANEL_STATUS_CHANGED 0x04
-#define CMD_GET_PANEL_STATUS_RESPONSE 0x05
-#define CMD_SET_BYPASSED_ZONE_LIST 0x06
-#define CMD_BYPASS_RESPONSE 0x07
-#define CMD_GET_ZONE_STATUS_RESPONSE 0x08
-// server receive
-#define CMD_ARM 0x00
-#define CMD_BYPASS 0x01
-#define CMD_EMERGENCY 0x02
-#define CMD_FIRE 0x03
-#define CMD_PANIC 0x04
-#define CMD_GET_ZONE_ID_MAP 0x05
-#define CMD_GET_ZONE_INFORMATION 0x06
-#define CMD_GET_PANEL_STATUS 0x07
-#define CMD_GET_BYPASSED_ZONE_LIST 0x08
-#define CMD_GET_ZONE_STATUS 0x09
-
 //  Arm mode command
 //-------------------
 // 0x00 Disarm    
@@ -75,12 +53,12 @@
 // 0x80-0xff Manufacturer specific
 
 const std::array<KeyMap, 7> RConfigArmModeValues = { { {QLatin1String("disarmed")}, {QLatin1String("armed_stay")}, {QLatin1String("armed_night")}, {QLatin1String("armed_away")},
-                                                        {QLatin1String("invalid_code")}, {QLatin1String("not_ready")}, {QLatin1String("already_disarmed")} } };
+                                                       {QLatin1String("invalid_code")}, {QLatin1String("not_ready")}, {QLatin1String("already_disarmed")} } };
 
 
 const std::array<KeyMap, 11> RConfigPanelValues = { { {QLatin1String("disarmed")}, {QLatin1String("armed_stay")}, {QLatin1String("armed_night")}, {QLatin1String("armed_away")},
-                                                       {QLatin1String("exit_delay")}, {QLatin1String("entry_delay")}, {QLatin1String("not_ready_to_arm")}}, {QLatin1String("in_alarm")},
-                                                       {QLatin1String("arming_stay")}, {QLatin1String("arming_night")}, {QLatin1String("arming_away")}};
+                                                      {QLatin1String("exit_delay")}, {QLatin1String("entry_delay")}, {QLatin1String("not_ready_to_arm")}}, {QLatin1String("in_alarm")},
+                                                      {QLatin1String("arming_stay")}, {QLatin1String("arming_night")}, {QLatin1String("arming_away")} } };
 
 const QStringList PanelStatusList({
     "disarmed","armed_stay","armed_night","armed_away","exit_delay","entry_delay","not_ready_to_arm","in_alarm","arming_stay","arming_night","arming_away"
--- ias_ace.h
+++ ias_ace.h
@@ -3,7 +3,27 @@
 
 #include "utils/utils.h"
 
-#define FAN_CTRL_ATTRID_FAN_MODE           0x0000
+// server send
+#define CMD_ARM_RESPONSE 0x00
+#define CMD_GET_ZONE_ID_MAP_RESPONSE 0x01
+#define CMD_GET_ZONE_INFORMATION_RESPONSE 0x02
+#define CMD_ZONE_STATUS_CHANGED 0x03
+#define CMD_PANEL_STATUS_CHANGED 0x04
+#define CMD_GET_PANEL_STATUS_RESPONSE 0x05
+#define CMD_SET_BYPASSED_ZONE_LIST 0x06
+#define CMD_BYPASS_RESPONSE 0x07
+#define CMD_GET_ZONE_STATUS_RESPONSE 0x08
+// server receive
+#define CMD_ARM 0x00
+#define CMD_BYPASS 0x01
+#define CMD_EMERGENCY 0x02
+#define CMD_FIRE 0x03
+#define CMD_PANIC 0x04
+#define CMD_GET_ZONE_ID_MAP 0x05
+#define CMD_GET_ZONE_INFORMATION 0x06
+#define CMD_GET_PANEL_STATUS 0x07
+#define CMD_GET_BYPASSED_ZONE_LIST 0x08
+#define CMD_GET_ZONE_STATUS 0x09
 
 extern const std::array<KeyMap, 7> RConfigArmModeValues;
 extern const std::array<KeyMap, 11> RConfigPanelValues;
