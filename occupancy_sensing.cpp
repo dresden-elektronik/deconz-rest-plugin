72939d0cc516824443cf456f0af1ea5d00955c33
SwoopX <swoop0711@gmx.de>
2021-06-28 14:25:44 +0200

    Consider PR comments

--- occupancy_sensing.cpp
+++ occupancy_sensing.cpp
@@ -38,230 +38,222 @@ void DeRestPluginPrivate::handleOccupancySensingClusterIndication(const deCONZ::
     {
         isReporting = true;
     }
-
-    // Read ZCL reporting and ZCL Read Attributes Response
-    if (isReadAttr || isReporting)
+    else
     {
-        const NodeValue::UpdateType updateType = isReadAttr ? NodeValue::UpdateByZclRead : NodeValue::UpdateByZclReport;
+        return; // neither ZCL Report nor ZCL Read Attributes Response
+    }
 
-        bool configUpdated = false;
-        bool stateUpdated = false;
+    const NodeValue::UpdateType updateType = isReadAttr ? NodeValue::UpdateByZclRead : NodeValue::UpdateByZclReport;
 
-        while (!stream.atEnd())
+    bool configUpdated = false;
+    bool stateUpdated = false;
+
+    while (!stream.atEnd())
+    {
+        quint16 attrId;
+        quint8 attrTypeId;
+
+        stream >> attrId;
+        if (isReadAttr)
         {
-            quint16 attrId;
-            quint8 attrTypeId;
-
-            stream >> attrId;
-            if (isReadAttr)
-            {
-                quint8 status;
-                stream >> status;  // Read Attribute Response status
-                if (status != deCONZ::ZclSuccessStatus)
-                {
-                    continue;
-                }
-            }
-            stream >> attrTypeId;
-
-            deCONZ::ZclAttribute attr(attrId, attrTypeId, QLatin1String(""), deCONZ::ZclRead, false);
-
-            if (!attr.readFromStream(stream))
+            quint8 status;
+            stream >> status;  // Read Attribute Response status
+            if (status != deCONZ::ZclSuccessStatus)
             {
                 continue;
             }
+        }
+        stream >> attrTypeId;
 
-            ResourceItem *item = nullptr;
+        deCONZ::ZclAttribute attr(attrId, attrTypeId, QLatin1String(""), deCONZ::ZclRead, false);
 
-            switch (attrId)
+        if (!attr.readFromStream(stream))
+        {
+            continue;
+        }
+
+        ResourceItem *item = nullptr;
+
+        switch (attrId)
+        {
+        case OCCUPIED_STATE:
+        {
+            quint8 occupancy = attr.numericValue().u8;
+            item = sensor->item(RStatePresence);
+
+            if (item)
             {
-            case OCCUPIED_STATE:
-            {
-                quint8 occupancy = attr.numericValue().u8;
-                item = sensor->item(RStatePresence);
+                item->setValue(occupancy);
+                enqueueEvent(Event(RSensors, RStatePresence, sensor->id(), item));
+                stateUpdated = true;
 
-                if (item)
+                const NodeValue &val = sensor->getZclValue(OCCUPANCY_SENSING_CLUSTER_ID, OCCUPIED_STATE);
+
+                // prepare to automatically set presence to false
+                if (item->toBool())
                 {
-                    item->setValue(occupancy);
-                    enqueueEvent(Event(RSensors, RStatePresence, sensor->id(), item));
-                    stateUpdated = true;
-
-                    const NodeValue &val = sensor->getZclValue(OCCUPANCY_SENSING_CLUSTER_ID, OCCUPIED_STATE);
-
-                    // prepare to automatically set presence to false
-                    if (item->toBool())
+                    if (val.maxInterval > 0 && updateType == NodeValue::UpdateByZclReport)
                     {
-                        if (val.maxInterval > 0 && updateType == NodeValue::UpdateByZclReport)
+                        // prevent setting presence back to false, when report.maxInterval > config.duration
+                        sensor->durationDue = item->lastSet().addSecs(val.maxInterval);
+                    }
+                    else
+                    {
+                        ResourceItem *item2 = sensor->item(RConfigDuration);
+                        if (item2 && item2->toNumber() > 0)
                         {
-                            // prevent setting presence back to false, when report.maxInterval > config.duration
-                            sensor->durationDue = item->lastSet().addSecs(val.maxInterval);
-                        }
-                        else
-                        {
-                            ResourceItem *item2 = sensor->item(RConfigDuration);
-                            if (item2 && item2->toNumber() > 0)
-                            {
-                                sensor->durationDue = item->lastSet().addSecs(item2->toNumber());
-                            }
+                            sensor->durationDue = item->lastSet().addSecs(item2->toNumber());
                         }
                     }
                 }
-
-                sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, OCCUPIED_STATE, attr.numericValue());
             }
-                break;
 
-            case OCCUPIED_TO_UNOCCUPIED_DELAY:
+            sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, OCCUPIED_STATE, attr.numericValue());
+        }
+            break;
+
+        case OCCUPIED_TO_UNOCCUPIED_DELAY:
+        {
+            if (sensor->modelId().startsWith(QLatin1String("FLS-NB")) || sensor->modelId() == QLatin1String("LG IP65 HMS"))
             {
-                if (sensor->modelId().startsWith(QLatin1String("FLS-NB")) || sensor->modelId() == QLatin1String("LG IP65 HMS"))
+                quint16 duration = attr.numericValue().u16;
+                item = sensor->item(RConfigDuration);
+
+                if (!item) { item = sensor->addItem(DataTypeUInt16, RConfigDuration); }
+
+                if (item && item->toNumber() != duration)
                 {
-                    quint16 duration = attr.numericValue().u16;
-                    item = sensor->item(RConfigDuration);
+                    enqueueEvent(Event(RSensors, RConfigDuration, sensor->id(), item));
 
-                    if (!item) { item = sensor->addItem(DataTypeUInt16, RConfigDuration); }
-
-                    if (item && item->toNumber() != duration)
+                    if (item->toNumber() <= 0)
                     {
-                        enqueueEvent(Event(RSensors, RConfigDuration, sensor->id(), item));
-
-                        if (item->toNumber() <= 0)
-                        {
-                            DBG_Printf(DBG_INFO, "got occupied to unoccupied delay %u\n", duration);
-                            item->setValue(duration);
-                            configUpdated = true;
-                        }
-                        else
-                        {
-                            DBG_Printf(DBG_INFO, "occupied to unoccupied delay is %u should be %u, force rewrite\n", duration, (quint16)item->toNumber());
-                            if (!sensor->mustRead(WRITE_OCCUPANCY_CONFIG))
-                            {
-                                sensor->enableRead(WRITE_OCCUPANCY_CONFIG);
-                                sensor->setNextReadTime(WRITE_OCCUPANCY_CONFIG, queryTime);
-                                queryTime = queryTime.addSecs(1);
-                            }
-
-                            if (!sensor->mustRead(READ_OCCUPANCY_CONFIG))
-                            {
-                                sensor->enableRead(READ_OCCUPANCY_CONFIG);
-                                sensor->setNextReadTime(READ_OCCUPANCY_CONFIG, queryTime);
-                                queryTime = queryTime.addSecs(5);
-                            }
-                            //Q_Q(DeRestPlugin);
-                            //q->startZclAttributeTimer(q->checkZclAttributesDelay);
-
-                            Q_Q(DeRestPlugin);
-                            q->startZclAttributeTimer(checkZclAttributesDelay);
-                        }
-                    }
-                }
-                else
-                {
-                    quint16 delay = attr.numericValue().u16;
-                    item = sensor->item(RConfigDelay);
-
-                    if (item && item->toNumber() != delay)
-                    {
-                        item->setValue(delay);
-                        enqueueEvent(Event(RSensors, RConfigDelay, sensor->id(), item));
+                        DBG_Printf(DBG_INFO, "got occupied to unoccupied delay %u\n", duration);
+                        item->setValue(duration);
                         configUpdated = true;
                     }
-
-                    if (sensor->mustRead(WRITE_DELAY))
+                    else
                     {
-                        ResourceItem *item = sensor->item(RConfigPending);
-                        if (item)
+                        DBG_Printf(DBG_INFO, "occupied to unoccupied delay is %u should be %u, force rewrite\n", duration, (quint16)item->toNumber());
+                        if (!sensor->mustRead(WRITE_OCCUPANCY_CONFIG))
                         {
-                            quint16 mask = item->toNumber();
-                            mask &= ~R_PENDING_DELAY;
-                            item->setValue(mask);
-                            enqueueEvent(Event(RSensors, RConfigPending, sensor->id(), item));
+                            sensor->enableRead(WRITE_OCCUPANCY_CONFIG);
+                            sensor->setNextReadTime(WRITE_OCCUPANCY_CONFIG, queryTime);
+                            queryTime = queryTime.addSecs(1);
                         }
-                        sensor->clearRead(WRITE_DELAY);
+
+                        if (!sensor->mustRead(READ_OCCUPANCY_CONFIG))
+                        {
+                            sensor->enableRead(READ_OCCUPANCY_CONFIG);
+                            sensor->setNextReadTime(READ_OCCUPANCY_CONFIG, queryTime);
+                            queryTime = queryTime.addSecs(5);
+                        }
+                        //Q_Q(DeRestPlugin);
+                        //q->startZclAttributeTimer(q->checkZclAttributesDelay);
+
+                        Q_Q(DeRestPlugin);
+                        q->startZclAttributeTimer(checkZclAttributesDelay);
                     }
                 }
-
-                sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, OCCUPIED_TO_UNOCCUPIED_DELAY, attr.numericValue());
             }
-                break;
-
-            case HUE_SENSITIVITY:
+            else
             {
-                NodeValue &val = sensor->getZclValue(OCCUPANCY_SENSING_CLUSTER_ID, HUE_SENSITIVITY);
-                // allow proper binding checks
-                if (val.minInterval == 0 || val.maxInterval == 0)
-                {
-                    val.minInterval = 5;      // value used by Hue bridge
-                    val.maxInterval = 7200;   // value used by Hue bridge
-                }
+                quint16 delay = attr.numericValue().u16;
+                item = sensor->item(RConfigDelay);
 
-                quint8 sensitivity = attr.numericValue().u8;
-                item = sensor->item(RConfigSensitivity);
-
-                if (item && item->toNumber() != sensitivity)
+                if (item && item->toNumber() != delay)
                 {
-                    item->setValue(sensitivity);
-                    enqueueEvent(Event(RSensors, RConfigSensitivity, sensor->id(), item));
+                    item->setValue(delay);
+                    enqueueEvent(Event(RSensors, RConfigDelay, sensor->id(), item));
                     configUpdated = true;
                 }
 
-                if (sensor->mustRead(WRITE_SENSITIVITY))
+                if (sensor->mustRead(WRITE_DELAY))
                 {
                     ResourceItem *item = sensor->item(RConfigPending);
                     if (item)
                     {
                         quint16 mask = item->toNumber();
-                        mask &= ~R_PENDING_SENSITIVITY;
+                        mask &= ~R_PENDING_DELAY;
                         item->setValue(mask);
-                        Event e(RSensors, RConfigPending, sensor->id(), item);
-                        enqueueEvent(e);
+                        enqueueEvent(Event(RSensors, RConfigPending, sensor->id(), item));
                     }
-                    sensor->clearRead(WRITE_SENSITIVITY);
+                    sensor->clearRead(WRITE_DELAY);
                 }
-
-                sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, HUE_SENSITIVITY, attr.numericValue());
             }
-                break;
 
-            case HUE_SENSITIVITY_MAX:
+            sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, OCCUPIED_TO_UNOCCUPIED_DELAY, attr.numericValue());
+        }
+            break;
+
+        case HUE_SENSITIVITY:
+        {
+            NodeValue &val = sensor->getZclValue(OCCUPANCY_SENSING_CLUSTER_ID, HUE_SENSITIVITY);
+            // allow proper binding checks
+            if (val.minInterval == 0 || val.maxInterval == 0)
             {
-                quint8 sensitivitymax = attr.numericValue().u8;
-                item = sensor->item(RConfigSensitivityMax);
+                val.minInterval = 5;      // value used by Hue bridge
+                val.maxInterval = 7200;   // value used by Hue bridge
+            }
 
-                if (item && item->toNumber() != sensitivitymax)
+            quint8 sensitivity = attr.numericValue().u8;
+            item = sensor->item(RConfigSensitivity);
+
+            if (item && item->toNumber() != sensitivity)
+            {
+                item->setValue(sensitivity);
+                enqueueEvent(Event(RSensors, RConfigSensitivity, sensor->id(), item));
+                configUpdated = true;
+            }
+
+            if (sensor->mustRead(WRITE_SENSITIVITY))
+            {
+                ResourceItem *item = sensor->item(RConfigPending);
+                if (item)
                 {
-                    item->setValue(sensitivitymax);
-                    enqueueEvent(Event(RSensors, RConfigSensitivityMax, sensor->id(), item));
-                    configUpdated = true;
+                    quint16 mask = item->toNumber();
+                    mask &= ~R_PENDING_SENSITIVITY;
+                    item->setValue(mask);
+                    Event e(RSensors, RConfigPending, sensor->id(), item);
+                    enqueueEvent(e);
                 }
-
-                sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, HUE_SENSITIVITY_MAX, attr.numericValue());
+                sensor->clearRead(WRITE_SENSITIVITY);
             }
-                break;
 
-            default:
-                break;
+            sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, HUE_SENSITIVITY, attr.numericValue());
+        }
+            break;
+
+        case HUE_SENSITIVITY_MAX:
+        {
+            quint8 sensitivitymax = attr.numericValue().u8;
+            item = sensor->item(RConfigSensitivityMax);
+
+            if (item && item->toNumber() != sensitivitymax)
+            {
+                item->setValue(sensitivitymax);
+                enqueueEvent(Event(RSensors, RConfigSensitivityMax, sensor->id(), item));
+                configUpdated = true;
             }
-        }
 
-        if (stateUpdated)
-        {
-            sensor->updateStateTimestamp();
-            enqueueEvent(Event(RSensors, RStateLastUpdated, sensor->id()));
+            sensor->setZclValue(updateType, ind.srcEndpoint(), OCCUPANCY_SENSING_CLUSTER_ID, HUE_SENSITIVITY_MAX, attr.numericValue());
         }
+            break;
 
-        if (configUpdated || stateUpdated)
-        {
-            updateSensorEtag(&*sensor);
-            sensor->setNeedSaveDatabase(true);
-            queSaveDb(DB_SENSORS, DB_SHORT_SAVE_DELAY);
+        default:
+            break;
         }
     }
 
-    if (!isReporting)
+    if (stateUpdated)
     {
-        if (!(zclFrame.frameControl() & deCONZ::ZclFCDisableDefaultResponse) && ind.dstAddress().isNwkUnicast())
-        {
-            sendZclDefaultResponse(ind, zclFrame, deCONZ::ZclSuccessStatus);
-        }
+        sensor->updateStateTimestamp();
+        enqueueEvent(Event(RSensors, RStateLastUpdated, sensor->id()));
+    }
+
+    if (configUpdated || stateUpdated)
+    {
+        updateSensorEtag(&*sensor);
+        sensor->setNeedSaveDatabase(true);
+        queSaveDb(DB_SENSORS, DB_SHORT_SAVE_DELAY);
     }
 }
