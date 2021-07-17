e3b5fece4c15def60de4bc83dc360db6b45b20ab
Smanar <aurelien.pra@laposte.net>
2021-07-17 19:12:01 +0200

    Use Model id

--- bindings.cpp
+++ bindings.cpp
@@ -2591,9 +2591,7 @@ void DeRestPluginPrivate::checkLightBindingsForAttributeReporting(LightNode *lig
     
     // Hack : Need to disable reporting for thoses devices, else It will enable a auto power off after 2mn.
     // see https://github.com/dresden-elektronik/deconz-rest-plugin/issues/3693
-    if (lightNode->manufacturer() == QLatin1String("_TZ3000_fvh3pjaz") ||
-        lightNode->manufacturer() == QLatin1String("_TZ3000_wyhuocal") ||
-        lightNode->manufacturer() == QLatin1String("_TZ3000_9hpxg80k "))
+    if if (R_GetProductId(&lightNode).endsWith(QLatin1String("(T)")))
     {
         return;
     }
