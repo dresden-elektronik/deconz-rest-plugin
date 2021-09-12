97652104a1cbf3decdf20c7d105806739f40b775
Smanar <aurelien.pra@laposte.net>
2021-09-12 19:12:07 +0200

    First test

--- de_web_plugin_private.h
+++ de_web_plugin_private.h
@@ -507,6 +507,7 @@ extern const quint64 silabs7MacPrefix;
 extern const quint64 silabs8MacPrefix;
 extern const quint64 silabs9MacPrefix;
 extern const quint64 silabs10MacPrefix;
+extern const quint64 telinkMacPrefix;
 extern const quint64 instaMacPrefix;
 extern const quint64 boschMacPrefix;
 extern const quint64 jennicMacPrefix;
