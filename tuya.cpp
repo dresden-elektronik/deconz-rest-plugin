12f14077ef9f1aa39300c71400740e4915a9e51c
Smanar <aurelien.pra@laposte.net>
2021-09-19 11:27:42 +0200

    Update tuya.cpp

--- tuya.cpp
+++ tuya.cpp
@@ -1392,12 +1392,12 @@ void DeRestPluginPrivate::handleTuyaClusterIndication(const deCONZ::ApsDataIndic
             sensorNode->updateStateTimestamp();
             enqueueEvent(Event(RSensors, RStateLastUpdated, sensorNode->id()));
             
-            //In some situation the reachable flag star at false
+            //In some situation the reachable flag stay at false
             ResourceItem *item3 = sensorNode->item(RConfigReachable);
             if (item3 && !item3->toBool())
             {
                 item3->setValue(true);
-                enqueueEvent(Event(RSensors, RConfigReachable, sensor->id(), item3));
+                enqueueEvent(Event(RSensors, RConfigReachable, sensorNode->id(), item3));
             }
 
             sensorNode->setNeedSaveDatabase(true);
