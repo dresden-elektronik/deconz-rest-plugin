7b19765749fe640628f97f22ad8df407b2a283b3
Smanar <aurelien.pra@laposte.net>
2021-10-02 22:23:54 +0200

    Update de_web_plugin.cpp

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -5345,6 +5345,7 @@ void DeRestPluginPrivate::addSensorNode(const deCONZ::Node *node, const deCONZ::
                             manufacturer.endsWith(QLatin1String("oclfnxz")) ||
                             manufacturer.endsWith(QLatin1String("ivfvd7h")) ||
                             manufacturer.endsWith(QLatin1String("wnjrr72")) ||
+                            manufacturer.endsWith(QLatin1String("s5v5jor")) ||
                             manufacturer.endsWith(QLatin1String("GbxAXL2")))
                         {
                             fpThermostatSensor.inClusters.push_back(TUYA_CLUSTER_ID);
@@ -5917,6 +5918,7 @@ void DeRestPluginPrivate::addSensorNode(const deCONZ::Node *node, const deCONZ::
                         manufacturer.endsWith(QLatin1String("w7cahqs")) ||
                         manufacturer.endsWith(QLatin1String("wdxldoj")) ||
                         manufacturer.endsWith(QLatin1String("hn3negr")) ||
+                        manufacturer.endsWith(QLatin1String("s5v5jor")) ||
                         manufacturer.endsWith(QLatin1String("88teujp")))
                     {
                         fpThermostatSensor.inClusters.push_back(TUYA_CLUSTER_ID);
