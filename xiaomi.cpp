a87f27b965f6cbf031823f811693bd29633a4bc6
Arye Levin <arye.l@freetail.com>
2021-10-17 21:35:12 +0300

    Added log

--- xiaomi.cpp
+++ xiaomi.cpp
@@ -1114,6 +1114,8 @@ void DeRestPluginPrivate::handleZclAttributeReportIndicationXiaomiSpecial(const 
  */
 void DeRestPluginPrivate::handleZclAttributeReportIndicationXiaomiAqaraS1ScenePanelSpecial(const deCONZ::ApsDataIndication &ind, deCONZ::ZclFrame &zclFrame)
 {
+    DBG_Printf(DBG_INFO, "0x%016llX Xiaomi Aqara attribute report\n", ind.srcAddress().ext());
+
     quint16 attrId = 0;
     quint8 dataType = 0;
     quint8 length = 0;
