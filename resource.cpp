dresden-elektronik/deconz-rest-plugin

commit babe2ac60b4ad698f7b486ef929138b21075f3ec

Refactor of changeSensorConfig

resource.cpp
@@ -159,8 +159,10 @@ const char *RConfigPulseConfiguration = "config/pulseconfiguration";
 const char *RConfigPreset = "config/preset";
 const char *RConfigMelody = "config/melody";
 const char *RConfigVolume = "config/volume";
-const char *RConfigTempThreshold = "config/temperaturethreshold";
-const char *RConfigHumiThreshold = "config/humiditythreshold";
+const char *RConfigTempMaxThreshold = "config/temperaturemaxthreshold";
+const char *RConfigTempMinThreshold = "config/temperatureminthreshold";
+const char *RConfigHumiMaxThreshold = "config/humiditymaxthreshold";
+const char *RConfigHumiMinThreshold = "config/humidityminthreshold";
 const char *RConfigReachable = "config/reachable";
 const char *RConfigSchedule = "config/schedule";
 const char *RConfigScheduleOn = "config/schedule_on";
@@ -338,8 +340,10 @@ void initResourceDescriptors()
     rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeString, RConfigPreset));
     rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeUInt8, RConfigMelody));
     rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeUInt8, RConfigVolume));
-    rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeString, RConfigTempThreshold));
-    rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeString, RConfigHumiThreshold));
+    rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeInt8, RConfigTempMaxThreshold));
+    rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeInt8, RConfigTempMinThreshold));
+    rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeInt8, RConfigHumiMaxThreshold));
+    rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeInt8, RConfigHumiMinThreshold));
     rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeBool, RConfigReachable));
     rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeString, RConfigSchedule));
     rItemDescriptors.emplace_back(ResourceItemDescriptor(DataTypeBool, RConfigScheduleOn));

resource.h
@@ -164,8 +164,10 @@ extern const char *RConfigPowerOnLevel;
 extern const char *RConfigPulseConfiguration;
 extern const char *RConfigPreset;
 extern const char *RConfigMelody;
-extern const char *RConfigTempThreshold;
-extern const char *RConfigHumiThreshold;
+extern const char *RConfigTempMaxThreshold;
+extern const char *RConfigTempMinThreshold;
+extern const char *RConfigHumiMaxThreshold;
+extern const char *RConfigHumiMinThreshold;
 extern const char *RConfigVolume;
 extern const char *RConfigReachable;
 extern const char *RConfigSchedule;

database.cpp
@@ -3379,8 +3379,10 @@ static int sqliteLoadAllSensorsCallback(void *user, int ncols, char **colval , c
                 sensor.addItem(DataTypeUInt8, RConfigMelody);
                 sensor.addItem(DataTypeString, RConfigPreset);
                 sensor.addItem(DataTypeUInt8, RConfigVolume);
-                sensor.addItem(DataTypeString, RConfigTempThreshold);
-                sensor.addItem(DataTypeString, RConfigHumiThreshold);
+                sensor.addItem(DataTypeInt8, RConfigTempMaxThreshold);
+                sensor.addItem(DataTypeInt8, RConfigTempMinThreshold);
+                sensor.addItem(DataTypeInt8, RConfigHumiMaxThreshold);
+                sensor.addItem(DataTypeInt8, RConfigHumiMinThreshold);
             }
 
         }

de_web_plugin.cpp
@@ -6717,8 +6717,10 @@ void DeRestPluginPrivate::addSensorNode(const deCONZ::Node *node, const SensorFi
             sensorNode.addItem(DataTypeUInt8, RConfigMelody);
             sensorNode.addItem(DataTypeString, RConfigPreset);
             sensorNode.addItem(DataTypeUInt8, RConfigVolume);
-            sensorNode.addItem(DataTypeString, RConfigTempThreshold);
-            sensorNode.addItem(DataTypeString, RConfigHumiThreshold);
+            sensorNode.addItem(DataTypeInt8, RConfigTempMaxThreshold);
+            sensorNode.addItem(DataTypeInt8, RConfigTempMinThreshold);
+            sensorNode.addItem(DataTypeInt8, RConfigHumiMaxThreshold);
+            sensorNode.addItem(DataTypeInt8, RConfigHumiMinThreshold);
         }
     }
     else if (sensorNode.type().endsWith(QLatin1String("CarbonMonoxide")))

tuya.cpp
@@ -654,7 +654,9 @@ void DeRestPluginPrivate::handleTuyaClusterIndication(const deCONZ::ApsDataIndic
                     case 0x026C : // max alarm temperature threshold
                     {
                         qint8 min = static_cast<qint8>(data & 0xFF);
-                        ResourceItem *item = sensorNode->item(RConfigTempThreshold);
+                        // Hack to get the code compiled quick and dirty
+                        // ResourceItem *item = sensorNode->item(RConfigTempThreshold);
+                        ResourceItem *item = sensorNode->item(RConfigTempMaxThreshold);
 
                         if (item)
                         {
@@ -684,7 +686,7 @@ void DeRestPluginPrivate::handleTuyaClusterIndication(const deCONZ::ApsDataIndic
                             DBG_Printf(DBG_INFO, "Tuya debug 34 : %s\n", qPrintable(valuesList.join(',')));
                             
                             item->setValue(valuesList.join(','));
-                            Event e(RSensors, RConfigTempThreshold, sensorNode->id(), item);
+                            Event e(RSensors, RConfigTempMaxThreshold, sensorNode->id(), item);
                             enqueueEvent(e);
                             
                             update = true;
@@ -695,7 +697,9 @@ void DeRestPluginPrivate::handleTuyaClusterIndication(const deCONZ::ApsDataIndic
                     case 0x026E : // max alarm humidity threshold
                     {
                         qint8 min = static_cast<qint8>(data & 0xFF);
-                        ResourceItem *item = sensorNode->item(RConfigHumiThreshold);
+                        // Hack to get the code compiled quick and dirty
+                        // ResourceItem *item = sensorNode->item(RConfigHumiThreshold);
+                        ResourceItem *item = sensorNode->item(RConfigHumiMaxThreshold);
 
                         if (item)
                         {
@@ -721,7 +725,7 @@ void DeRestPluginPrivate::handleTuyaClusterIndication(const deCONZ::ApsDataIndic
                             if (dp == 0x026E) { valuesList[1] = QString::number(min); }
                             
                             item->setValue(valuesList.join(','));
-                            Event e(RSensors, RConfigHumiThreshold, sensorNode->id(), item);
+                            Event e(RSensors, RConfigHumiMaxThreshold, sensorNode->id(), item);
                             enqueueEvent(e);
                             
                             update = true;

utils/utils.h
@@ -12,7 +12,37 @@
 #define UTILS_H
 
 #include <QString>
+#include <QVariant>
+#include <array>
+
+struct KeyValMap {
+    QLatin1String key;
+    quint8 value;
+    };
+    
+struct KeyValMapInt {
+    quint8 key;
+    quint16 value;
+    };
+
+struct KeyValMapTuyaSingle {
+    QLatin1String key;
+    char value[1];
+    };
 
 QString generateUniqueId(quint64 extAddress, quint8 endpoint, quint16 clusterId);
+
+template <typename K, typename Cont>
+decltype(auto) getMappedValue2(const K &key, const Cont &cont)
+{
+    typename Cont::value_type ret{};
+    const auto res = std::find_if(cont.cbegin(), cont.cend(), [&key](const auto &i){ return i.key == key; });
+    if (res != cont.cend())
+    {
+        ret = *res;
+    }
+
+    return ret;
+}
 
 #endif // UTILS_H

commit 81a646e3a130e2f9582841ddb3ec4b26ce8a134c

Fix update sensor config/group via REST-API
Config group is uses to specify the group ids used by group bindings.
This affects switches like Ubisys C4/S1/S2 where the group list is configured via REST-API.

Regression from: babe2ac60b4ad698f7b486ef929138b21075f3ec

resource.cpp
@@ -1469,3 +1469,25 @@ QLatin1String R_DataTypeToString(ApiDataType type)
 
     return QLatin1String("unknown");
 }
+
+/*! Returns true if \p str contains a valid list of group identifiers.
+
+    Valid values are:
+      ""          empty
+      "45"        single group
+      "343,123"   two groups
+ */
+bool isValidRConfigGroup(const QString &str)
+{
+    int result = 0;
+    const QStringList groupList = str.split(',', SKIP_EMPTY_PARTS);
+
+    for (const auto &groupId : groupList)
+    {
+        bool ok = false;
+        auto gid = groupId.toUInt(&ok, 0);
+        if (ok && gid <= UINT16_MAX) { result++; }
+    }
+
+    return result == groupList.size();
+}

resource.h
@@ -588,6 +588,8 @@ bool R_SetValueEventOnSet(Resource *r, const char *suffix, const V &val, Resourc
     return result;
 }
 
+bool isValidRConfigGroup(const QString &str);
+
 uint8_t DDF_GetSubDeviceOrder(const QString &type);
 QLatin1String R_DataTypeToString(ApiDataType type);
 inline bool isValid(Resource::Handle hnd) { return hnd.hash != 0 && hnd.index < UINT16_MAX && hnd.type != 0; }

rest_sensors.cpp
@@ -1678,8 +1678,12 @@ int DeRestPluginPrivate::changeSensorConfig(const ApiRequest &req, ApiResponse &
                 }
                 else if (rid.suffix == RConfigGroup) // String
                 {
-                    checkSensorBindingsForClientClusters(sensor);
-                    continue;
+                    data.valid = isValidRConfigGroup(data.string);
+                    if (data.valid)
+                    {
+                        updated = true;
+                        checkSensorBindingsForClientClusters(sensor);
+                    }
                 }
                 else if (QString(rid.suffix).startsWith("config/ubisys_j1_")) // Unsigned integer
                 {

utils/utils.cpp
@@ -173,6 +173,11 @@ RestData verifyRestData(const ResourceItemDescriptor &rid, const QVariant &val)
                 data.valid = true;
                 return data;
             }
+            else if (rid.suffix == RConfigGroup)
+            {
+                data.valid = true; // empty string allowed
+                return data;
+            }
             else
             {
                 return data;