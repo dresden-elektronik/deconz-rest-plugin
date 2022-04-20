6e67ce51023dbfa50b5d306bc734c538a46a52db
SwoopX <swoop0711@gmx.de>
2022-04-20 23:34:28 +0200

    Correct Danfoss window states script

--- devices/danfoss/danfoss_windowopen_state.js
+++ devices/danfoss/danfoss_windowopen_state.js
@@ -1,17 +1,17 @@
 switch (Attr.val) {
-    case "0":
+    case 0:
         Item.val = "Quarantine";
         break;
-    case "1":
+    case 1:
         Item.val = "Closed";
         break;
-    case "2":
+    case 2:
         Item.val = "Hold";
         break;
-    case "3":
+    case 3:
         Item.val = "Open";
         break;
-    case "4":
+    case 4:
         Item.val = "Open (external), closed (internal)";
         break;
 }
