25c7bb317f6c96367f5059143e5c208c1efd0d1f
Smanar <aurelien.pra@laposte.net>
2022-04-30 17:42:40 +0200

    add set preset

--- devices/tuya/tuya_trv_preset.js
+++ devices/tuya/tuya_trv_preset.js
@@ -1,23 +1,23 @@
 switch (Attr.val) {
-    case "0":
+    case 0:
         Item.val = "holiday";
         break;
-    case "1":
+    case 1:
         Item.val = "auto";
         break;
-    case "2":
+    case 2:
         Item.val = "manual";
         break;
-    case "3":
+    case 3:
         Item.val = "comfort";
         break;
-    case "4":
+    case 4:
         Item.val = "eco";
         break;
-    case "5":
+    case 5:
         Item.val = "boost";
         break;
-    case "6":
+    case 6:
         Item.val = "complex";
         break;
 }
