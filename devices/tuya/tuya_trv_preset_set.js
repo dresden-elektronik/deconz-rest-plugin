fa345aa47acae12e648347ce21e423dbe6c238c3
Smanar <aurelien.pra@laposte.net>
2022-05-16 17:58:16 +0200

    Update tuya_trv_preset_set.js

--- devices/tuya/tuya_trv_preset_set.js
+++ devices/tuya/tuya_trv_preset_set.js
@@ -1,3 +1,3 @@
-const modes = ["holiday","auto","manual","comfort","eco","boost","complex"];
-v = modes.indexOf(Attr.val);
-if (v >= 0) { Item.val = modes.indexOf(Attr.val); }
+const modes = ['holiday','auto','manual','comfort','eco','boost','complex'];
+v = modes.indexOf(Item.val);
+if (v >= 0) { v }
