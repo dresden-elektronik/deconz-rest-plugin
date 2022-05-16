6eeefd0b0b9c3659b7d958d385cd568504eed754
Smanar <aurelien.pra@laposte.net>
2022-05-16 18:06:25 +0200

    Update tuya_trv_preset.js

--- devices/tuya/tuya_trv_preset.js
+++ devices/tuya/tuya_trv_preset.js
@@ -1,23 +1,23 @@
 switch (Attr.val) {
     case 0:
-        Item.val = "holiday";
+        Item.val = 'holiday';
         break;
     case 1:
-        Item.val = "auto";
+        Item.val = 'auto';
         break;
     case 2:
-        Item.val = "manual";
+        Item.val = 'manual';
         break;
     case 3:
-        Item.val = "comfort";
+        Item.val = 'comfort';
         break;
     case 4:
-        Item.val = "eco";
+        Item.val = 'eco';
         break;
     case 5:
-        Item.val = "boost";
+        Item.val = 'boost';
         break;
     case 6:
-        Item.val = "complex";
+        Item.val = 'complex';
         break;
 }
