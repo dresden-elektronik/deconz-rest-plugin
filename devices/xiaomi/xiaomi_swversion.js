0d78586addced20aeeb8089ecd41ec234b483fec
SwoopX <swoop0711@gmx.de>
2022-07-09 21:50:31 +0200

    Consider data type in FW string creation

--- devices/xiaomi/xiaomi_swversion.js
+++ devices/xiaomi/xiaomi_swversion.js
@@ -1,7 +1,18 @@
-const v = (Attr.val & 0x00ff);
 let s = "0.0.0_";
+let v = 0;
+
+if (Attr.dataType == 33)
+{
+    v = (Attr.val & 0x00ff);
+}
+else if (Attr.dataType == 35)
+{
+    v = (Attr.val & 0x000000ff);
+}
+
 for (i = 0; i < (v.toString().length % 4); i++)
 {
     s += "0";
 }
+
 Item.val = s + v;
