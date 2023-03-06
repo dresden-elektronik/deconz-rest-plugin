9c68317ab20d5b3dc75df8deb3bbaa893a84b016
Arye Levin <aryelevin@me.com>
2023-03-07 00:41:52 +0200

    another try

--- xiaomi.cpp
+++ xiaomi.cpp
@@ -1305,7 +1305,7 @@ void DeRestPluginPrivate::handleZclAttributeReportIndicationXiaomiAqaraS1ScenePa
                 map["r"] = QLatin1String("lights");
                 map["id"] = lightNode.id();
                 map["uniqueid"] = lightNode.uniqueId();
-                map[item->descriptor().suffix] = stringParam;
+                map["aqara_s1_panel_communication"] = stringParam;
                 webSocketServer->broadcastTextMessage(Json::serialize(map));
                 updateLightEtag(&lightNode);
                 // plugin->saveDatabaseItems |= DB_LIGHTS;
