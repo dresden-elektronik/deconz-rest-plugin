9d32541e610a6318856a932bb424247d284fd7a9
Arye Levin <aryelevin@me.com>
2023-04-09 02:57:11 +0300

    try

--- device_js/device_js_wrappers.cpp
+++ device_js/device_js_wrappers.cpp
@@ -105,6 +105,7 @@ QVariant JsResourceItem::value() const
 
     if (type == DataTypeString || type == DataTypeTime ||  type == DataTypeTimePattern)
     {
+                DBG_Printf(DBG_DDF, "String is %s\n", qPrintable(citem->toString()));
         return citem->toString();
     }
 
