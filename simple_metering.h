b414c83853602b679396ca59f903233b7d7ba529
SwoopX <swoop0711@gmx.de>
2023-04-13 20:55:56 +0200

    Fix removal of still required simple metering attribute

--- simple_metering.h
+++ simple_metering.h
@@ -5,6 +5,7 @@
 
 //Attribute IDs
 #define METERING_ATTRID_CURRENT_SUMMATION_DELIVERED             0x0000
+#define METERING_ATTRID_PULSE_CONFIGURATION                     0x0300  // Develco specific
 #define METERING_ATTRID_INTERFACE_MODE                          0x0302  // Develco specific
 #define METERING_ATTRID_INSTANTANEOUS_DEMAND                    0x0400
 
