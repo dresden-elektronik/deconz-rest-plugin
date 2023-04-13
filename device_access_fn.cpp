4b53103803692706fdc77099a8b03254aa097241
Arye Levin <aryelevin@me.com>
2023-04-13 16:39:28 +0300

    a try

--- device_access_fn.cpp
+++ device_access_fn.cpp
@@ -1811,6 +1811,9 @@ bool writeZclAttribute(const Resource *r, const ResourceItem *item, deCONZ::ApsC
             {
                 const auto res = engine.result();
                 DBG_Printf(DBG_DDF, "%s/%s expression: %s --> %s\n", r->item(RAttrUniqueId)->toCString(), item->descriptor().suffix, qPrintable(expr), qPrintable(res.toString()));
+                if (dataType == deCONZ::ZclOctedString) {
+                    res = QByteArray::fromHex(res.toLatin1());
+                }
                 attribute.setValue(res);
             }
             else
