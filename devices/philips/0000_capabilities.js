1ac20a2ff711bb66f573705ef9d084abb2c6115e
ebaauw <erik.baauw@xs4all.nl>
2023-05-05 11:58:40 +0200

    Update 0000_capabilities.js
    Only update item when real productname has been found.  Use the dummy assignment, to see offset in deCONZ log.

--- devices/philips/0000_capabilities.js
+++ devices/philips/0000_capabilities.js
@@ -6,6 +6,7 @@ const offset = ZclFrame.at(3) << 8 | ZclFrame.at(2)
 // const totalLength = ZclFrame.at(7) << 8 | ZclFrame.at(6)
 // const length = ZclFrame.at(10) // ostring length
 var o = 11 // start of ostring data bytes
+var s = ''
 
 if (status === 0x0000) {
   if (offset === 0x0000) {
@@ -16,14 +17,13 @@ if (status === 0x0000) {
         if (ZclFrame.at(o++) === 0x12) { // manufacturername
           o += ZclFrame.at(o) + 1
           if (ZclFrame.at(o) === 0x42) { // productname
-            Item.val = 'offset: 0x' + ('00000000' + (Number(o) - 11).toString(16)).slice(-8)
+            s = 'offset: 0x' + ('00000000' + (Number(o) - 11).toString(16)).slice(-8)
           }
         }
       }
     }
   } else {
     if (ZclFrame.at(o++) === 0x42) { // T: productname
-      var s = ''
       for (var l = ZclFrame.at(o++); l > 0; l--) {
         s += String.fromCharCode(ZclFrame.at(o++))
       }
