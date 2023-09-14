17f26e2902d838d81ec5329eef6efb43cccec9ac
Zehir <zehir@zorim.fr>
2023-09-14 13:14:20 +0200

    Update etag when removing user fix #7216 (#7217)

--- rest_configuration.cpp
+++ rest_configuration.cpp
@@ -2311,6 +2311,8 @@ int DeRestPluginPrivate::deleteUser(const ApiRequest &req, ApiResponse &rsp)
             rsp.list.append(rspItem);
             rsp.httpStatus = HttpStatusOk;
 
+            updateEtag(gwConfigEtag);
+
             return REQ_READY_SEND;
         }
     }
