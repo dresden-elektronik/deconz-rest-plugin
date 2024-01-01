34fe5667e7105ccd633c845a63ac10b3cff2bbf3
Smanar <aurelien.pra@laposte.net>
2024-01-01 11:41:21 +0100

    Update de_web_plugin.cpp

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -2356,7 +2356,7 @@ void DeRestPluginPrivate::addLightNode(const deCONZ::Node *node)
     auto *device = DEV_GetOrCreateDevice(this, deCONZ::ApsController::instance(), eventEmitter, m_devices, node->address().ext());
     Q_ASSERT(device);
 
-    if (false && permitJoinFlag)
+    if ((node->nodeDescriptor().manufacturerCode() != VENDOR_PROFALUX) && permitJoinFlag)
     {
         // during pairing only proceed when device code has finished query Basic Cluster
         if (device->item(RAttrManufacturerName)->toString().isEmpty() ||
