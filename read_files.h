bee5d599186557b18bb9f94a5e06fb9add093385
Manuel Pietschmann <mpi@dresden-elektronik.de>
2025-03-29 15:12:46 +0100

    Refactor button maps (part 1) (#8149)
    No functionality change, only flatten data structures and remove Qt. All strings turned into atoms. Processing is slightly faster and uses less memory.

--- read_files.h
+++ read_files.h
@@ -6,10 +6,10 @@
 
 QJsonDocument readButtonMapJson(const QString &path);
 bool checkRootLevelObjectsJson(const QJsonDocument &buttonMaps, const QStringList &requiredObjects);
-QMap<QString, quint16> loadButtonMapClustersJson(const QJsonDocument &buttonMaps);
-QMap<QString, QMap<QString, quint16>> loadButtonMapCommadsJson(const QJsonDocument &buttonMaps);
+std::vector<ButtonCluster> loadButtonMapClustersJson(const QJsonDocument &buttonMaps);
+std::vector<ButtonClusterCommand> loadButtonMapCommadsJson(const QJsonDocument &buttonMaps);
 std::vector<ButtonProduct> loadButtonMapModelIdsJson(const QJsonDocument &buttonMapsDoc, const std::vector<ButtonMap> &buttonMaps);
 std::vector<ButtonMeta> loadButtonMetaJson(const QJsonDocument &buttonMapsDoc, const std::vector<ButtonMap> &buttonMaps);
-std::vector<ButtonMap> loadButtonMapsJson(const QJsonDocument &buttonMaps, const QMap<QString, quint16> &btnMapClusters, const QMap<QString, QMap<QString, quint16>> &btnMapClusterCommands);
+std::vector<ButtonMap> loadButtonMapsJson(const QJsonDocument &buttonMaps, const std::vector<ButtonCluster> &btnMapClusters, const std::vector<ButtonClusterCommand> &btnMapClusterCommands);
 
 #endif // READ_FILES_H
