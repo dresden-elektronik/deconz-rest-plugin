dd8391b5c5995505c80bed0ca8e98edc66728d57
Manuel Pietschmann <mpi@dresden-elektronik.de>
2025-05-06 15:05:52 +0200

    Fix don't expose `ddf_hash` and `ddf_policy` for lights and sensors
    These are only part of `/devices` endpoint. Also removes another attribute which can be `null` for lights and sensors.

--- rest_sensors.cpp
+++ rest_sensors.cpp
@@ -2691,6 +2691,12 @@ bool DeRestPluginPrivate::sensorToMap(Sensor *sensor, QVariantMap &map, const Ap
                 continue;
             }
 
+            // following are only exposed on /devices
+            if (rid.suffix == RAttrDdfHash || rid.suffix == RAttrDdfPolicy)
+            {
+                continue;
+            }
+
             const ApiAttribute a = rid.toApi(map, event);
             QVariantMap *p = a.map;
             (*p)[a.key] = item->toVariant();
