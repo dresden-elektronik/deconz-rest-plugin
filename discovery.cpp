f324ab41fee9dff9d4047309d5bb18b25a6ac659
Manuel Pietschmann <mpi@dresden-elektronik.de>
2025-11-13 13:33:30 +0100

    Announce HTTPS port if active (Discovery and UPnP) (#8397)
    The description.xml got and extra element `deconz:info` and within there if HTTPS is active a sub element `httpsPort`.
    
    The UPnP UDP broadcast got the extra field "HTTPS.phoscon.de: <https-port>".
    
    The HTTPS port gets also announced to the discovery server. Note currently not visible as processing needs to be added server side there soon.

--- discovery.cpp
+++ discovery.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 dresden elektronik ingenieurtechnik gmbh.
+ * Copyright (c) 2016-2025 dresden elektronik ingenieurtechnik gmbh.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -192,6 +192,11 @@ void DeRestPluginPrivate::internetDiscoveryTimerFired()
     map["mac"] = gwBridgeId;
     map["internal_ip"] = gwConfig["ipaddress"].toString();
     map["internal_port"] = gwConfig["port"].toDouble();
+    uint16_t httpsPort = ctrl->getParameter(deCONZ::ParamHttpsPort);
+    if (httpsPort > 0)
+    {
+        map["https_port"] = (double)httpsPort;
+    }
     map["interval"] = gwAnnounceInterval;
     map["swversion"] = gwConfig["swversion"].toString();
     map["fwversion"] = gwConfig["fwversion"].toString();
